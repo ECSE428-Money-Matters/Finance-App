9742e51f7206ed355bb3e175284b9c2e4320bc88
karimkanafani <karim.kanafani@mail.mcgill.ca>
2023-10-23 23:27:37 -0400

    Integrated account recovery into index.js. Modified account_recovery.js to use users table.

--- MoneyMatters/Backend/index.js
+++ MoneyMatters/Backend/index.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const pool = require("./db");
 const {rows} = require("pg/lib/defaults");
 
-const create_user = require('./create-user')
+const create_user = require('./create-user');
+const login = require('./login.js');
+const recover = require('./account_recovery.js')
 
 // Middleware
 app.use(cors());
@@ -24,79 +26,7 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.use(create_user)
-
-const login = require('./login.js')
-
 app.use(login);
-
-app.use((req, res, next) => {
-    console.log(`Received ${req.method} request on ${req.path}`);
-    next();
-});
-
-// Create an example
-app.post("/examples", async (req,res) => {
-    try {
-        const { description } = req.body// req.body contains the json description data of the example created
-        const newExample = await pool.query(
-            "INSERT INTO exampletable (description) VALUES($1) RETURNING *",
-            [description]
-        );
-
-        res.json(newExample.rows[0]);
-    }catch (err){
-        console.error(err.message);
-    }
-});
-// Get all examples
-
-app.get("/examples", async (req,res) =>{
-    try {
-        const allExamples = await pool.query("SELECT * FROM exampletable");
-        res.json(allExamples.rows);
-    }catch (err){
-        console.error((err.message));
-    }
-});
-// Get an example
-
-app.get("/examples/:id", async (req, res) =>{
-    try {
-        const {id} = req.params;
-        const example = await pool.query("SELECT * FROM exampletable WHERE example_id = $1", [id]);
-        res.json(example.rows[0]);
-    }catch (err){
-        console.error(err.message);
-    }
-})
-// Update an example
-
-app.put("/examples/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        const {description} = req.body;
-        const updateExample = await pool.query(
-            "UPDATE exampletable SET description = $1 WHERE example_id = $2",
-            [description, id]
-        );
-        res.json("Example was updated");
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-// Delete an example
-app.delete("/examples/:id", async (req, res) =>{
-    try {
-        const {id} = req.params;
-        const deleteExample = await pool.query(
-            "DELETE FROM exampletable WHERE example_id = $1",
-            [id]
-        );
-        res.json("Example was deleted");
-    } catch (err) {
-        console.error(err.message);
-    }
-});
+app.use(recover);
 
 module.exports = app;
