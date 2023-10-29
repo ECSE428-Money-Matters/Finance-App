8f7c76fa5e1e5fdb26f476da6149278055d99579
karimkanafani <karim.kanafani@mail.mcgill.ca>
2023-10-29 19:33:08 -0400

    UI for Add and View expense is done. Awaiting backend to do the calls

---
+++ MoneyMatters/Frontend/components/Expense.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Expense = (props) => {
+
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemsLeft}>
+                <Text style={styles.expenseText}>{props.desc}</Text>
+            </View>
+            <View>
+                <Text style={styles.amountText}>{props.amt}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 15,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+    itemsLeft: {
+        maxWidth: '80%',
+
+    },
+    expenseText: {
+        color: '#457B9D'
+    },
+    amountText: {
+        color: '#E63946'
+    }
+
+
+});
+export default Expense;
--- MoneyMatters/Frontend/pages/Dashboard.js
+++ MoneyMatters/Frontend/pages/Dashboard.js
@@ -1,95 +1,189 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, TextInput, Button, Text } from 'react-native';
+import React, {useState} from 'react';
+import {
+    View,
+    StyleSheet,
+    TextInput,
+    Button,
+    Text,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Keyboard, ScrollView
+} from 'react-native';
+import {Colors} from "react-native/Libraries/NewAppScreen";
+import Expense from "../components/Expense";
 
-const Dashboard = ({ navigation}) => {
-  const [code, setCode] = useState('');
+const Dashboard = ({navigation}) => {
+    const [expenseDescription, setExpenseDescription] = useState('');
+    const [expenseAmount, setExpenseAmount] = useState('');
 
-  const handleCode = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:3000/verify', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: email,
-          code: code
-        })
-      });
-
-      const responseBody = await response.text();
-      const message = JSON.parse(responseBody); // Parse the JSON response
-
-      if (message.message === "Account created successfully!") {
-        // handle successful login, e.g., navigate to a dashboard
-        navigation.navigate('Dashboard');  
-      } else {
-        // handle unsuccessful login, e.g., display an error message
-        alert(message);  
-      }
-    } catch (error) {
-      // handle error, e.g., network error or server error
-      console.error("Error during verification: ", error);
-      alert("Failed to connect to the server.");
+    const clearText = () => {
+        setExpenseAmount('');
+        setExpenseDescription('');
     }
-  };
-  
-  const handleSignUpRedirect = () => {   
-    navigation.navigate('SignUp');
-  };
 
-  const handlePasswordRecoveryRedirect = () => {
-    navigation.navigate('PasswordRecovery');
-  };
+    const submitButton = () => {
+        handleAddExpense();
+        clearText();
+        Keyboard.dismiss();
+    }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>This is Dashboard. Nothing to show yet:</Text>
-      <TextInput 
-        style={styles.input} 
-        value={code}
-        onChangeText={setCode}
-        placeholder="Enter your code"
-        secureTextEntry={false}
-      />
+    const handleAddExpense = async () => {
+        try {
+            console.log("Added expense");
+            console.log("Description: " + expenseDescription);
+            console.log("Amount: " + expenseAmount);
+            // const response = await fetch('http://127.0.0.1:3000/login', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     },
+            //     body: JSON.stringify({
+            //
+            //     })
+            // });
 
-      <Button title="Verify" onPress={handleCode} />
+            // const responseBody = await response.text();
+            // const message = JSON.parse(responseBody); // Parse the JSON response
+            //
+            // if (message === "Login successful") {
+            //     // handle successful login, e.g., navigate to a dashboard
+            //     navigation.navigate('Dashboard');
+            // } else {
+            //     // handle unsuccessful login, e.g., display an error message
+            //     alert(message);
+            // }
+        } catch (error) {
+            // handle error, e.g., network error or server error
+            // console.error("Error adding expense:", error);
+        }
+    };
 
-      <Text style={styles.linkText} onPress={handleSignUpRedirect}>   {/* <-- New "Sign Up" link */}
-        Don't have an account? Sign Up
-      </Text>
+    function renderHeader() {
+        return (
+            <View>
+                <View>
+                    <Text style={styles.header} >My Expenses</Text>
+                    <Text style={styles.subheader}>Summary</Text>
+                </View>
+            </View>
+        )
+    }
+    function renderExpenses() {
+        return (
+            <ScrollView>
+                <View style={styles.expenses}>
+                    <Expense desc={'Ex1'} amt={25}/>
+                    <Expense desc={'Ex2'} amt={25}/>
+                </View>
+            </ScrollView>
+        )
+    }
 
-      <Text style={styles.linkText} onPress={handlePasswordRecoveryRedirect}>
-        Forgot Password?
-      </Text>
-      
-    </View>
-  );
+    function renderBottomUI() {
+        return (
+            <KeyboardAvoidingView style={styles.bottomContainer} behavior={"padding"} enabled={true}>
+                <TextInput
+                    style={styles.expenseDescription}
+                    value={expenseDescription}
+                    onChangeText={setExpenseDescription}
+                    placeholder="Enter your expense desccription"
+                />
+                <TextInput
+                    style={styles.expenseAmount}
+                    value={expenseAmount}
+                    onChangeText={setExpenseAmount}
+                    placeholder="Amount"
+                    keyboardType={"numeric"}
+                />
+
+                <TouchableOpacity onPress={submitButton}>
+                    <Text style={styles.addButton}>+</Text>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            {renderHeader()}
+            {renderBottomUI()}
+            {renderExpenses()}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 15,
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 8,
-    marginBottom: 15,
-    borderRadius: 5,
-  },
-  linkText: {   // <-- New style for "Sign Up" link
-    color: 'blue',
-    textAlign: 'center',
-    marginTop: 10,
-    textDecorationLine: 'underline',
-  },
+    container: {
+        flex: 1,
+        paddingHorizontal: 15,
+        paddingVertical: 75
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 8,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 8,
+        marginBottom: 15,
+        borderRadius: 5,
+    },
+    linkText: {   // <-- New style for "Sign Up" link
+        color: 'blue',
+        textAlign: 'center',
+        marginTop: 10,
+        textDecorationLine: 'underline',
+    },
+    header: {
+        color: Colors.primary,
+        fontSize: 35
+    },
+    subheader: {
+        color: '#1D3557',
+        fontSize: 20
+    },
+    expenseDescription: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 8,
+        marginBottom: 15,
+        borderRadius: 5,
+        height: 35,
+        width: 250
+    },
+    expenseAmount: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 8,
+        marginBottom: 15,
+        borderRadius: 5,
+        height: 35,
+        width: 70
+    },
+    addButton: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 8,
+        marginBottom: 15,
+        borderRadius: 5,
+        height: 35,
+        width: 25
+    },
+    bottomContainer: {
+        flex: 1,
+        paddingHorizontal: 15,
+        paddingVertical: 75,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        position: 'absolute',
+        bottom: 20,
+        left: 0,
+        right: 0,
+        alignItems: 'center'
+    }, expenses: {
+        paddingVertical: 20
+    },
 });
 
 export default Dashboard;
