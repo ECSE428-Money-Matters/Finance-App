f21524c7c9556d8e9391a5cc493f67022ffd8bc3
mehdilimitless <mammor@student.unimelb.edu.au>
2023-10-30 19:27:07 -0400

    Added more view_expenses tests

--- MoneyMatters/Backend/test/unit_tests/view_expenses.test.js
+++ MoneyMatters/Backend/test/unit_tests/view_expenses.test.js
@@ -27,10 +27,52 @@ describe('Viewing Expenses', () => {
         expect(res.body.length).toBe(expenses.length);
     });
 
+    test('should return an empty list when there are no expenses', async () => {
+        // Simulate a scenario where there are no expenses in the database
+        dbStub.returns(Promise.resolve({ rows: [] }));
 
-    // Mohammed add here additional tests
-    test('handle errors when fetching expenses', async () => {
         const res = await request(app).get('/view_expenses');
-        expect(res.status).toBe(500); // Example: Check for a 500 internal server error
+
+        expect(res.status).toBe(200);
+        expect(res.body.length).toBe(0);
     });
+
+    test('should return expenses for a specific category', async () => {
+        // Simulate a scenario where you want to filter expenses by category
+        const categoryToFilter = 'Food';
+        const expenses = [
+            { expense_name: 'Expense 1', amount: 100, category: 'Food' },
+            { expense_name: 'Expense 2', amount: 200, category: 'Food' },
+        ];
+
+        dbStub.returns(Promise.resolve({ rows: expenses }));
+
+        const res = await request(app).get(`/view_expenses?category=${categoryToFilter}`);
+
+        expect(res.status).toBe(200);
+        expect(res.body.length).toBe(expenses.length);
+        expect(res.body.every(expense => expense.category === categoryToFilter)).toBe(true);
+    });
+
+    test('should return expenses in a specific date range', async () => {
+        // Simulate a scenario where you want to filter expenses by date range
+        const startDate = '2023-10-01';
+        const endDate = '2023-10-31';
+        const expenses = [
+            { expense_name: 'Expense 1', amount: 100, category: 'Food', posted_date: '2023-10-15' },
+            { expense_name: 'Expense 2', amount: 200, category: 'Transportation', posted_date: '2023-10-20' },
+        ];
+
+        dbStub.returns(Promise.resolve({ rows: expenses }));
+
+        const res = await request(app).get(`/view_expenses?start_date=${startDate}&end_date=${endDate}`);
+
+        expect(res.status).toBe(200);
+        expect(res.body.length).toBe(expenses.length);
+        expect(res.body.every(expense => {
+            const date = new Date(expense.posted_date);
+            return date >= new Date(startDate) && date <= new Date(endDate);
+        })).toBe(true);
+    });
+    // Mohammed add here additional test
 });
